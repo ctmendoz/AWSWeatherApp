ca916907cc3b99faa7424285e09d945ac139a7e2
ctmendoz <78514228+ctmendoz@users.noreply.github.com>
2023-07-07 12:48:58 -0500

    Add files via upload

---
+++ EBSApp/config.py
@@ -0,0 +1,12 @@
+# The link from EC2(?) that leads to the web app deployed in EBS
+customhost = "a3weather.c63ypvgtwsuo.us-east-1.rds.amazonaws.com"
+# The DB Instance Master Username (from RDS)
+customuser = ""
+# The DB Instance Master Password (from RDS)
+custompass = ""
+# The name of the DB Instance (from RDS)
+customdb = "a3weather"
+# The name of the image bucket made with S3
+custombucket = "a3weatherdata"
+# The AWS region the web app was made/ deployed in
+customregion = "us-east-1"
